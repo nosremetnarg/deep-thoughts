3337a2101a21babca199e368a5b4f8fa096b7efc
nosremetnarg <grant.alex.emerson@gmail.com>
2020-10-19 21:00:37 -0500

    invalid token

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,8 +4,16 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
-type Thought {
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
@@ -21,21 +29,24 @@ type Thought {
     username: String
   }
 
-  type User {
-    _id: ID
-    username: String
-    email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
-  }
-
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+  
 `;
 
 // export the typeDefs
