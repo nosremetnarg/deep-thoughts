050a3a7509f98f8925236a6ae020b546379fb5bf
nosremetnarg <grant.alex.emerson@gmail.com>
2020-10-21 12:31:31 -0500

    logout functionality and local token storage working

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const secret = 'mysecretsshhhhh';
-const expiration = '12h';
+const expiration = '1200h';
 
 module.exports = {
     signToken: function ({ username, email, _id }) {
